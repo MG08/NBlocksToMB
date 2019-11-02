2c1251693929349b869e7d28ae6b370c819c5ed3
M. Garcia <miguel_itecit@hotmail.com>
2019-11-02 18:43:51 +0000

    v1.2d

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module NBlocksConverter
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+      config.assets.precompile += %w(*.js)
       config.assets.enabled = true
       config.serve_static_assets = true
 
