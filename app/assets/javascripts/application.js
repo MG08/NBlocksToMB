f2934fcd358bd33d198f12c4afd0274e840e8a36
M. Garcia <miguel_itecit@hotmail.com>
2019-11-02 19:01:19 +0000

    v1.2e

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,32 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require mycode
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
-
-
-$(document).ready(function(){
-    $('#a1').keyup(calculate);
-    $('#a4').keyup(calculate);
-    $('#a7').keyup(calculate);
-
-    var http = require("http");
-		setInterval(function() {
-    	http.get("http://nblocksconverter.herokuapp.com");
-}, 300000); // every 5 minutes (300000)
-
-});
-
-function calculate(e)
-{
-    console.log('this is an Input');
-    $('#a2').val($('#a1').val() /8);
-    $('#a3').val($('#a1').val() /8192);
-    $('#a5').val($('#a4').val() /1024);
-    $('#a6').val($('#a4').val() *8);
-    $('#a8').val($('#a7').val() *1024);
-    $('#a9').val($('#a7').val() *8192);
-}
 
